84646c46a8cf78d5c21f72bc55e7532eb506731a
RaihanSnh <nathahamzahraihansatya@gmail.com>
2023-09-14 23:23:58 +0700

    TogglePassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from './assets/pages/layout';
+import Login from './assets/pages/auth/Login';
 
 function App() {
   return (
     <>
-      <Layout/>
+      <Login/>
     </>
   );
 }
