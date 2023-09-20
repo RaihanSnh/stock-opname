883791d64b284d3c4ec76b790608abdf78800a3f
RaihanSnh <nathahamzahraihansatya@gmail.com>
2023-09-21 00:01:08 +0700

    ToggleSidebar

--- src/assets/component/Sidebar.jsx
+++ src/assets/component/Sidebar.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 
 function Sidebar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.classList.toggle("sidebar-open", isOpen);
+    }, [isOpen]);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
 
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <aside className="w-64 h-full bg-blue-500">
-            <div className="p-2 border-b h-20">
+        <aside className={`sidebar ${isOpen ? "bg-sky-800" : ""} transition-all duration-300`} aria-controls="sidebar" aria-expanded={isOpen}>
+            <button className="absolute top-0 left-0 p-3" onClick={toggleSidebar}>
+                <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+                {/*ini svg diganti nanti jadi logo*/}
+            </button>
+            <div className="p-2 border-b h-20 flex items-center">
                 <div className="py-4 px-3">
-                    <Link to="/" className="md:py-2 text-white text-lg font-semibold sm:text-2xl">
+                    <Link to="/" className="md:py-2 text-white text-lg font-semibold sm:text-2xl ml-5">
                         <b>STOCK</b> Opname
                     </Link>
                 </div>
@@ -59,4 +74,4 @@ function Sidebar() {
     );
 }
 
-export default Sidebar;
+export default Sidebar
