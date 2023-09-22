e841a9014292ca9f1de57d58d472d2b0396ffd59
RaihanSnh <nathahamzahraihansatya@gmail.com>
2023-09-22 12:43:27 +0700

    complete responsive sidebar animation

--- src/assets/component/Sidebar.jsx
+++ src/assets/component/Sidebar.jsx
@@ -3,17 +3,33 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.classList.toggle("sidebar-open", isOpen);
+    }, [isOpen]);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
 
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <aside className="w-64 h-full bg-blue-500">
-            <div className="p-2 border-b h-20">
+        <aside className={`${isOpen ? "w-64" : "w-16"} bg-sky-800 transition-width duration-300`} aria-controls="sidebar" aria-expanded={isOpen}>
+            <button className="absolute top-0 left-0 p-3" onClick={toggleSidebar}>
+                <svg className={`w-${isOpen ? "6" : "16" } h-${isOpen ? "6" : "16" } text-gray-800`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+                {/*ini svg diganti nanti jadi logo*/}
+            </button>
+            <div className="p-2 border-b h-20 flex items-center">
                 <div className="py-4 px-3">
                     <Link to="/" className={`md:py-2 text-white text-lg font-semibold sm:text-2xl ml-${isOpen ? "5" : "2"}`}>
-                        <b>STOCK</b> Opname
+                        <b style={{ display: isOpen ? "inline" : "none" }}>STOCK</b>{" "}
+                        <span style={{ display: isOpen ? "inline" : "none" }}>Opname</span>
                     </Link>
                 </div>
             </div>
@@ -59,4 +75,4 @@ function Sidebar() {
     );
 }
 
-export default Sidebar;
+export default Sidebar
