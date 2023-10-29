a007a518f1b224f0cbe8914be71a33f4e7f27086
RaihanSnh <nathahamzahraihansatya@gmail.com>
2023-10-29 17:20:23 +0700

    [Conflict] Fix

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,21 +1,21 @@
-import React, { useContext, useState } from 'react';
-import axios from 'axios';
-import Cookies from 'universal-cookie';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../App';
-import {EyeIconOpen, EyeIconClosed} from "../../assets/images/icon/icon";
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../App";
+import { EyeIconOpen, EyeIconClosed } from "../../assets/images/icon/icon";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const coockies = new Cookies();
-  const {setAuth} = useContext(AuthContext);
+  const { setAuth } = useContext(AuthContext);
 
   const togglePasswordVisibility = () => {
-    setShowPassword(prevShowPassword => !prevShowPassword);
+    setShowPassword((prevShowPassword) => !prevShowPassword);
   };
 
   const handleEmail = (e) => {
@@ -26,125 +26,115 @@ export default function Login() {
     setPassword(e.target.value);
   };
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleRedirect = (role) => {
     switch (role) {
-      case 'admin':
-        navigate('/dashboard/admin/barang');
+      case "admin":
+        navigate("/dashboard/admin/barang");
         break;
-      case 'warehouse_staff':
-        navigate('/dashboard/staff/barang');
+      case "warehouse_staff":
+        navigate("/dashboard/staff/barang");
         break;
-      case 'requester':
-        navigate('/dashboard/requester');
+      case "requester":
+        navigate("/dashboard/requester");
         break;
     }
-  }
+  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
     setLoading(true);
 
+    axios.get("http://127.0.0.1:8000/sanctum/csrf-cookie");
 
-    axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie');
-
-    axios.post('http://127.0.0.1:8000/api/auth/login', {
-      email,
-      password
-    }, {
-      withCredentials: true
-    })
-<<<<<<< HEAD
-        .then((response) => {
-          coockies.set('Authorization', response.data.token);
-          localStorage.setItem('auth', JSON.stringify(response.data.token));
-          axios.get('http://127.0.0.1:8000/api/auth/getuser', {
-            headers : {
-              Authorization: `Bearer ${response.data.token}`
-            }
-          })
-              .then(response => {
-                setAuth(response.data.user);
-                console.log(response)
-                const role = response.data.user.role;
-                handleRedirect(role);
-              });
-        })
-        .catch((error) => {
-          console.error(error);
-          setMessage(error.response.data.message);
-          setLoading(false);
-=======
-    .then((response) => {
-      coockies.set('Authorization', response.data.token);
-      localStorage.setItem('auth', JSON.stringify(response.data.token));
-      localStorage.setItem('activeLink', 'barang')
-      axios.get('http://127.0.0.1:8000/api/auth/getuser', {
-        headers : {
-          Authorization: `Bearer ${response.data.token}`
+    axios
+      .post(
+        "http://127.0.0.1:8000/api/auth/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
         }
->>>>>>> ebc820347168070c121a4f6469d24f5a7fa95e5d
-        })
+      )
+      .then((response) => {
+        coockies.set("Authorization", response.data.token);
+        localStorage.setItem("auth", JSON.stringify(response.data.token));
+        axios
+          .get("http://127.0.0.1:8000/api/auth/getuser", {
+            headers: {
+              Authorization: `Bearer ${response.data.token}`,
+            },
+          })
+          .then((response) => {
+            setAuth(response.data.user);
+            console.log(response);
+            const role = response.data.user.role;
+            handleRedirect(role);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+        setMessage(error.response.data.message);
+        setLoading(false);
+      });
   };
-
   return (
-<<<<<<< HEAD
-      <div className="bg-gray-50 flex items-center justify-center h-screen">
-        <div className="bg-white p-8 space-y-6 rounded-lg shadow-md w-96">
-          <h1 className="text-3xl font-bold text-gray-900">Login</h1>
-          <form className="space-y-4" onSubmit={handleLogin} method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-900">
-                Email
-              </label>
-              <input
-                  type="text"
-                  id="email"
-                  name="email"
-                  className="w-full py-1 border-b border-gray-300 text-gray-900 text-sm outline-none focus:border-black placeholder:text-gray-400"
-                  placeholder="user@gmail.com"
-                  value={email}
-                  onChange={handleEmail}
-              >
-              </input>
-            </div>
-            <div className="relative w-full inline-block">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-900">
-                Password
-              </label>
-              <input
-                  type={showPassword ? 'text' : 'password'}
-                  id="password"
-                  name="password"
-                  className="w-full py-1 border-b border-gray-300 text-gray-900 text-sm outline-none focus:border-black placeholder:text-gray-400"
-                  placeholder="••••••••"
-                  value={password}
-                  onChange={handlePassword}
-              />
-              <span
-                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer select-none"
-                  onClick={togglePasswordVisibility}
-              >
-              {showPassword ? <EyeIconOpen /> : <EyeIconClosed />}
-            </span>
-              <p className='text-red-900 text-sm italic'>{message}</p>
-            </div>
-=======
-    <div className="bg-gray-50 flex items-center justify-center fixed z-50x inset-0">
+    <div className="bg-gray-50 flex items-center justify-center h-screen">
       <div className="bg-white p-8 space-y-6 rounded-lg shadow-md w-96">
         <h1 className="text-3xl font-bold text-gray-900">Login</h1>
         <form className="space-y-4" onSubmit={handleLogin} method="POST">
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-900">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-900"
+            >
               Email
             </label>
->>>>>>> ebc820347168070c121a4f6469d24f5a7fa95e5d
             <input
-                type={loading ? "button" : "submit"}
-                value={loading ? ('Loading...') : ('Login')}
-                className="w-full bg-blue-500 text-white text-sm font-medium px-5 py-2.5 rounded hover:bg-blue-600"/>
-          </form>
-        </div>
+              type="text"
+              id="email"
+              name="email"
+              className="w-full py-1 border-b border-gray-300 text-gray-900 text-sm outline-none focus:border-black placeholder:text-gray-400"
+              placeholder="user@gmail.com"
+              value={email}
+              onChange={handleEmail}
+            ></input>
+          </div>
+          <div className="relative w-full inline-block">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Password
+            </label>
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              className="w-full py-1 border-b border-gray-300 text-gray-900 text-sm outline-none focus:border-black placeholder:text-gray-400"
+              placeholder="••••••••"
+              value={password}
+              onChange={handlePassword}
+            />
+            <span
+              className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer select-none"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? <EyeIconOpen /> : <EyeIconClosed />}
+            </span>
+            <p className="text-red-900 text-sm italic">{message}</p>
+          </div>
+          <div>
+            <input
+              type={loading ? "button" : "submit"}
+              value={loading ? "Loading..." : "Login"}
+              className="w-full bg-blue-500 text-white text-sm font-medium px-5 py-2.5 rounded hover:bg-blue-600"
+            />
+          </div>
+        </form>
       </div>
+    </div>
   );
 }
