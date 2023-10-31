b72b33628607c32f13ebabeebed3c44901746ac6
RaihanSnh <nathahamzahraihansatya@gmail.com>
2023-10-31 11:21:28 +0700

    [skeletonLoading & Search] completing more feature]

--- src/views/pages/admin/Kategori.jsx
+++ src/views/pages/admin/Kategori.jsx
@@ -1,15 +1,17 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom"
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom"
 
 export default function Kategori() {
     const [dataCategory, setDataCategory] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [search, setSearch] = useState("");
     let num = 1;
 
     const fetchData = async () => {
-        await axios.get('http://127.0.0.1:8000/api/admin/category', {
-        }).then(response => {
-            setDataCategory(response.data);
+        await axios.get('http://127.0.0.1:8000/api/admin/category', {}).then(response => {
+            setDataCategory(response.data)
+            setIsLoading(false);
         }).catch(error => {
             console.error(error);
         });
@@ -19,7 +21,7 @@ export default function Kategori() {
         fetchData();
     }, [])
 
-    return(
+    return (
         <>
             <div className="flex items-center justify-between">
                 <h1 className="text-2xl font-medium">Kategori</h1>
@@ -34,28 +36,123 @@ export default function Kategori() {
                         <label for="table-search" className="sr-only">Search</label>
                         <div className="relative">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                <svg className="w-4 h-4 text-gray-500" aria-hidden="true"
+                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                 </svg>
                             </div>
-                            <input type="text" className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 outline-none" placeholder="Search for items"/>
+                            <input
+                                type="text"
+                                className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 outline-none"
+                                placeholder="Search for items"
+                                onChange={(event) => setSearch(event.target.value)}
+                            />
                         </div>
                     </div>
                 </div>
-            </div>  
+            </div>
             <div className="w-full mt-4 overflow-auto scrollbar-gray">
-                <div className="grid grid-cols-2 p-2 text-xs font-bold text-gray-900 bg-gray-100 rounded mb-1 sticky top-0">
+                <div
+                    className="grid grid-cols-2 p-2 text-xs font-bold text-gray-900 bg-gray-100 rounded mb-1 sticky top-0">
                     <span>No</span>
                     <span>Nama</span>
                 </div>
-                {dataCategory.map((row) => (
-                    <div key={row.id} className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1">
-                        <span>{num++}</span>
-                        <Link to={`edit/${row.id}`}>
-                            <span>{row.name}</span>
-                        </Link>
-                    </div>
-                ))}
+                {isLoading ? (
+                    <>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                    </>
+                ) : (
+                    dataCategory.filter((row) => {
+                        if (search === "") {
+                            return row
+                        } else if (row.name.toLowerCase().includes(search.toLowerCase())) {
+                            return row
+                        }
+                    }).map((row) => (
+                        <div key={row.id} className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1">
+                            <span>{num++}</span>
+                            <Link to={`edit/${row.id}`}>
+                                <span>{row.name}</span>
+                            </Link>
+                        </div>
+                    ))
+                )}
             </div>
         </>
     )
--- src/views/pages/admin/table_gudang.jsx
+++ src/views/pages/admin/table_gudang.jsx
@@ -1,16 +1,17 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 export default function TableGudang() {
     const [dataGudang, setDataGudang] = useState([]);
-
+    const [isLoading, setIsLoading] = useState(true);
+    const [search, setSearch] = useState("");
     let num = 1;
 
     const fetchData = async () => {
-        axios.get('http://127.0.0.1:8000/api/admin/warehouse', {
-        }).then(response => {
+        await axios.get('http://127.0.0.1:8000/api/admin/warehouse', {}).then(response => {
             setDataGudang(response.data)
+            setIsLoading(false);
         }).catch(error => {
             console.error(error);
         });
@@ -20,7 +21,7 @@ export default function TableGudang() {
         fetchData();
     }, [])
 
-    return(
+    return (
         <>
             <div className="mt-4">
                 <div className="flex justify-between items-center">
@@ -29,28 +30,99 @@ export default function TableGudang() {
                         <label for="table-search" className="sr-only">Search</label>
                         <div className="relative mt-1">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                <svg className="w-4 h-4 text-gray-500" aria-hidden="true"
+                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                 </svg>
                             </div>
-                            <input type="text" className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 outline-none" placeholder="Search for items"/>
+                            <input
+                                type="text"
+                                className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 outline-none"
+                                placeholder="Search for items"
+                                onChange={(event) => setSearch(event.target.value)}
+                            />
                         </div>
                     </div>
                 </div>
-            </div>  
+            </div>
             <div className="w-full mt-4 overflow-auto scrollbar-gray">
-                <div className="grid grid-cols-2 p-2 text-xs font-bold text-gray-900 bg-gray-100 rounded mb-1 sticky top-0">
+                <div
+                    className="grid grid-cols-2 p-2 text-xs font-bold text-gray-900 bg-gray-100 rounded mb-1 sticky top-0">
                     <span>No</span>
                     <span>Nama</span>
                 </div>
-                {dataGudang.map((row) => (
-                    <div key={row.id} className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1">
-                        <span>{num++}</span>
-                        <Link to={`edit/${row.id}`}>
-                            <span>{row.name}</span>
-                        </Link>
-                    </div>
-                ))}
+                {isLoading ? (
+                    <>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                        <div className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1 animate-pulse">
+                            <div className="bg-gray-400 h-4 w-10"></div>
+                            <div className="bg-gray-400 h-4 w-40"></div>
+                        </div>
+                    </>
+                ) : (
+                    dataGudang.filter((row) => {
+                        if (search === "") {
+                            return row
+                        } else if (row.name.toLowerCase().includes(search.toLowerCase())) {
+                            return row
+                        }
+                    }).map((row) => (
+                        <div key={row.id} className="grid grid-cols-2 p-2 text-xs font-medium text-gray-900 mb-1">
+                            <span>{num++}</span>
+                            <Link to={`edit/${row.id}`}>
+                                <span>{row.name}</span>
+                            </Link>
+                        </div>
+                    ))
+                )}
             </div>
         </>
     )
